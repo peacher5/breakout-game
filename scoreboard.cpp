f6be37c1e63cb04c0bd685529f5de8d98d2ba6d2
Peranut Wattanakulsiri <peranut.w@ku.th>
2018-05-03 10:29:02 +0700

    Fix game crash cause access denied on read/write high score

--- scoreboard.cpp
+++ scoreboard.cpp
@@ -1,4 +1,5 @@
 #include <fstream>
+#include <iostream>
 #include <sstream>
 #include <iomanip>
 #include <ctime>
@@ -15,18 +16,21 @@ void ScoreBoard::read() {
     ifstream high_score_file;
     string line, score_txt, date_txt, time_txt;
 
+    n_lines = 0;
+
     high_score_file.open("high_score.txt");
 
     if (high_score_file.is_open()) {
         // Read text file line by line
-        for (n_lines = 0; getline(high_score_file, line) && n_lines < 10; n_lines++) {
+        for (; getline(high_score_file, line) && n_lines < 10; n_lines++) {
             istringstream iss(line);
             iss >> score_txt >> date_txt >> time_txt;
             scoreboard_[n_lines].date_time = date_txt + "  " + time_txt;
             scoreboard_[n_lines].score = score_txt;
         }
-
         high_score_file.close();
+    } else {
+        cout << "Error: Unable to create/open high_score.txt file." << endl;
     }
 }
 
@@ -48,20 +52,23 @@ void ScoreBoard::write(int score) {
 
     high_score_file.open("high_score.txt");
 
-    int i = 0, line = 0;
-    for (; i < n_lines && line < 10; i++, line++) {
-        if (!is_write && score >= stoi(scoreboard_[i].score) ) {
-            high_score_file << score << " " << date_time << endl;
-            is_write = true, line++;
+    if (high_score_file.is_open()) {
+        int i = 0, line = 0;
+        for (; i < n_lines && line < 10; i++, line++) {
+            if (!is_write && score >= stoi(scoreboard_[i].score) ) {
+                high_score_file << score << " " << date_time << endl;
+                is_write = true, line++;
+            }
+            if (line < 10)
+                high_score_file << scoreboard_[i].score << " " << scoreboard_[i].date_time << endl;
         }
-        if (line < 10)
-            high_score_file << scoreboard_[i].score << " " << scoreboard_[i].date_time << endl;
+        if (!is_write && line < 10) {
+            high_score_file << score << " " << date_time << endl;
+        }
+        high_score_file.close();
+    } else {
+        cout << "Error: Unable to create/open high_score.txt file." << endl;
     }
-    if (!is_write && line < 10) {
-        high_score_file << score << " " << date_time << endl;
-    }
-
-    high_score_file.close();
 
     read();
 }
